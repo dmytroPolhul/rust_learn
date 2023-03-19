553467023ebdacf4e6b4b7c739c78cf8857bd2f1
Dmytro Polhul <polhul.d@gmail.com>
2023-03-19 16:58:16 +0200

    Add files via upload

---
+++ hello_cargo/target/debug/deps/hello_cargo-851f06aa8bc56012.d
@@ -0,0 +1,5 @@
+/Users/work_user/WebstormProjects/untitled4/hello_cargo/target/debug/deps/hello_cargo-851f06aa8bc56012: src/main.rs
+
+/Users/work_user/WebstormProjects/untitled4/hello_cargo/target/debug/deps/hello_cargo-851f06aa8bc56012.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/work_user/WebstormProjects/untitled4/hello_cargo/target/debug/hello_cargo: /Users/work_user/WebstormProjects/untitled4/hello_cargo/src/main.rs
